753034af5383f68ea79dbafd900666317d0f4953
tomgiagtz <tom.giagtzoglou@gmail.com>
2018-12-16 14:41:36 -0500

    Done.

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
+source 'https://rubygems.org'
+
 gem 'activesupport'
 gem 'pry'
 gem 'rspec'
--- recurring_moment.rb
+++ recurring_moment.rb
@@ -1,5 +1,6 @@
 require 'active_support'
 require 'active_support/core_ext'
+require 'pry'
 
 class RecurringMoment
   def initialize(start:, interval:, period:)
@@ -9,22 +10,40 @@ class RecurringMoment
   end
 
   def match(date)
-    current = @start
+	current = @start
+	# date_diff = 0
+	while current <= date
+		# puts 'current:  ' + current.strftime('%D')
+		# puts 'date: ' + current.strftime('%D')
+		# puts current == date
+		if current == date
+        	return true
+      	end
 
-    while current < date
-      if current == date
-        return true
-      end
+	  if @period == 'monthly'
+		current = current.advance(months: @interval)
+		# if the start is the end of the month, and the current day is before that day, reset to the end of the month
+		if @start.end_of_day == @start.end_of_month && current.day < @start.day
+			current = current.end_of_month.beginning_of_day
+		end
 
-      if @period == 'monthly'
+
+		#didn't work for multiple months at a time
+		# current = current.advance(days: date_diff)
+		# date_diff = 0
+		# binding.pry
+
+		# if current.day != @start.day
+		# 	date_diff = current.day - @start.day
+		# end
+		
+      elsif @period =='weekly'
         current = current.advance(weeks: @interval)
-      elsif @period = 'weekly'
-        current = current.advance(months: @interval)
       elsif @period == 'daily'
         current = current.advance(days: @interval)
-      end
+	  end
     end
-
+	
     return false
   end
 end
